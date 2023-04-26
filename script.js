cbe80f05d125b776e6cff711928fe005f21684b7
Sraboni <srabonidatta9@gmail.com>
2023-04-26 10:59:04 -0500

    mynote

---
+++ script.js
@@ -0,0 +1,97 @@
+let addBtn = document.getElementById("add-btn");
+addBtn.addEventListener("click", function(e) {
+
+  let addTitle = document.getElementById("note-title");
+  let addTxt = document.getElementById("note-text");
+  
+    if (addTitle.value == "" || addTxt.value == "") {
+        return alert("Please add Note Title and Details")
+    }
+
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  addTitle.value = "";
+  showNotes();
+});
+
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    html += `
+        <div class="note">
+            <p class="note-counter">Note ${index + 1}</p>
+            <h3 class="note-title"> ${element.title} </h3>
+            <p class="note-text"> ${element.text}</p>
+            <button id="${index}"onclick="deleteNote(this.id)" class="note-btn">Delete Note</button>
+            <button id="${index}"onclick="editNote(this.id)" class="note-btn edit-btn">Edit Note</button>
+        </div>`;
+  });
+  let notesElement = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElement.innerHTML = html;
+  } else {
+    notesElement.innerHTML = `No Notes Yet! Add a note using the form above.`;
+  }
+}
+
+// delete 
+function deleteNote(index) {
+    let confirmDelete = confirm("Delete this note?");
+    if (confirmDelete == true) {
+        let notes = localStorage.getItem("notes");
+        if (notes == null) {
+            notesObj = [];
+        } else {
+            notesObj = JSON.parse(notes);
+        }
+
+        notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        showNotes();
+    }
+  
+}
+
+// Edit 
+function editNote(index) {
+    let notes = localStorage.getItem("notes");
+    let addTitle = document.getElementById("note-title");
+    let addTxt = document.getElementById("note-text");
+
+    if (addTitle.value !== "" || addTxt.value !== "") {
+      return alert("Please clear the form before editing a note")
+    } 
+
+    if (notes == null) {
+      notesObj = [];
+    } else {
+      notesObj = JSON.parse(notes);
+    }
+    
+    notesObj.findIndex((element, index) => {
+      addTitle.value = element.title;
+      addTxt.value = element.text;
+    })
+    notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        showNotes();
+}
+
+showNotes();
